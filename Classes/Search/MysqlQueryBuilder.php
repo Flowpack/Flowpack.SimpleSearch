d1a718a4a5dcf2784cf27331d18b0d847772d036
Karsten Dambekalns <karsten@dambekalns.de>
2020-01-13 14:50:53 +0100

    FEATURE: Implement match highlighting for MySQL

--- Classes/Search/MysqlQueryBuilder.php
+++ Classes/Search/MysqlQueryBuilder.php
@@ -235,15 +235,147 @@ class MysqlQueryBuilder implements QueryBuilderInterface
      * Produces a snippet with the first match result for the search term.
      *
      * @param string $searchword The search word
-     * @param integer $resultTokens The amount of tokens (words) to get surrounding the match hit. (defaults to 60)
+     * @param integer $resultTokens The amount of characters to get surrounding the match hit. (defaults to 200)
      * @param string $ellipsis added to the end of the string if the text was longer than the snippet produced. (defaults to "...")
      * @param string $beginModifier added immediately before the searchword in the snippet (defaults to <b>)
      * @param string $endModifier added immediately after the searchword in the snippet (defaults to </b>)
      * @return string
+     * @see https://github.com/boyter/php-excerpt
      */
-    public function fulltextMatchResult($searchword, $resultTokens = 60, $ellipsis = '...', $beginModifier = '<b>', $endModifier = '</b>'): string
+    public function fulltextMatchResult($searchword, $resultTokens = 200, $ellipsis = '...', $beginModifier = '<b>', $endModifier = '</b>'): string
     {
-        return '';
+        $searchword = trim($searchword);
+
+        $query = $this->buildQueryString();
+        $results = $this->indexClient->executeStatement($query, []);
+
+        if ($results === []) {
+            return '';
+        }
+
+        $matches = [];
+        foreach ($results[0] as $indexedFieldName => $indexedFieldContent) {
+            if (!empty($indexedFieldContent) && strpos($indexedFieldName, '_') !== 0) {
+                $matches[] = trim(strip_tags((string)$indexedFieldContent));
+            }
+        }
+        $matchContent = implode(' ', $matches);
+
+        $searchWordParts = explode(' ', $searchword);
+        $matchContent = preg_replace(
+            array_map(static function (string $searchWordPart) {
+                return sprintf('/(%s)/iu', preg_quote($searchWordPart, '/'));
+            }, $searchWordParts),
+            array_fill(0, count($searchWordParts), sprintf('%s$1%s', $beginModifier, $endModifier)),
+            $matchContent
+        );
+
+        $matchLength = strlen($matchContent);
+        if ($matchLength <= $resultTokens) {
+            return $matchContent;
+        }
+
+        $locations = $this->extractLocations($searchWordParts, $matchContent);
+        $snippetLocation = $this->determineSnippetLocation($locations, (int)($resultTokens / 3));
+
+        return $this->extractSnippet($resultTokens, $ellipsis, $matchLength, $snippetLocation, $matchContent);
+    }
+
+    /**
+     * find the locations of each of the words
+     * Nothing exciting here. The array_unique is required
+     * unless you decide to make the words unique before passing in
+     *
+     * @param array $words
+     * @param string $fulltext
+     * @return array
+     * @see https://github.com/boyter/php-excerpt
+     */
+    private function extractLocations(array $words, string $fulltext): array
+    {
+        $locations = [];
+        foreach ($words as $word) {
+            $loc = stripos($fulltext, $word);
+            while ($loc !== false) {
+                $locations[0] = $loc;
+                $loc = stripos($fulltext, $word, $loc + strlen($word));
+            }
+        }
+
+        $locations = array_unique($locations);
+
+        sort($locations);
+        return $locations;
+    }
+
+    /**
+     * Work out which is the most relevant portion to display
+     *
+     * This is done by looping over each match and finding the smallest distance between two found
+     * strings. The idea being that the closer the terms are the better match the snippet would be.
+     * When checking for matches we only change the location if there is a better match.
+     * The only exception is where we have only two matches in which case we just take the
+     * first as will be equally distant.
+     *
+     * @param array $locations
+     * @param int $relativePosition
+     * @return int
+     * @see https://github.com/boyter/php-excerpt
+     */
+    private function determineSnippetLocation(array $locations, int $relativePosition): int
+    {
+        $locationsCount = count($locations);
+        $smallestDiff = PHP_INT_MAX;
+
+        if ($locationsCount === 0) {
+            return 0;
+        }
+
+        $startPosition = $locations[0];
+        if ($locationsCount > 2) {
+            // skip the first as we check 1 behind
+            for ($i = 1; $i < $locationsCount; $i++) {
+                if ($i === $locationsCount - 1) { // at the end
+                    $diff = $locations[$i] - $locations[$i - 1];
+                } else {
+                    $diff = $locations[$i + 1] - $locations[$i];
+                }
+
+                if ($smallestDiff > $diff) {
+                    $smallestDiff = $diff;
+                    $startPosition = $locations[$i];
+                }
+            }
+        }
+
+        return $startPosition > $relativePosition ? $startPosition - $relativePosition : 0;
+    }
+
+    /**
+     * @param int $resultTokens
+     * @param string $ellipsis
+     * @param int $matchLength
+     * @param int $snippetLocation
+     * @param string $matchContent
+     * @return string
+     */
+    private function extractSnippet(int $resultTokens, string $ellipsis, int $matchLength, int $snippetLocation, string $matchContent): string
+    {
+        if ($matchLength - $snippetLocation < $resultTokens) {
+            $snippetLocation = (int)($snippetLocation - ($matchLength - $snippetLocation) / 2);
+        }
+
+        $snippet = substr($matchContent, $snippetLocation, $resultTokens);
+
+        if ($snippetLocation + $resultTokens < $matchLength) {
+            $snippet = substr($snippet, 0, strrpos($snippet, ' ')) . $ellipsis;
+        }
+
+        if ($snippetLocation !== 0) {
+            $snippet = $ellipsis . substr($snippet, strpos($snippet, ' ') + 1);
+        }
+
+        return $snippet;
     }
 
     /**
